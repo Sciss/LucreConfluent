5b5d3946251c1150a2493d534ebc021fd77dc868
Sciss <contact@sciss.de>
2011-04-02 19:00:22 +0100

    this doesn't look right

--- src/main/scala/de/sciss/confluent/test/TypeSafePointerTest.scala
+++ src/main/scala/de/sciss/confluent/test/TypeSafePointerTest.scala
@@ -4,34 +4,32 @@ object TypeSafePointerTest {
 
    trait Version
 
-   trait Ctx[ +V1 <: Version ] {
-      type V = V1 // <: Version
-   }
+   trait Ctx[ V1 <: Version ]
 
    type AnyCtx     = Ctx[ _ <: Version ]
    type AnyRf[ T ] = Ref[ _ <: Version, T ]
 
    object Ref {
-      implicit def access[ C <: AnyCtx, R, T ]( r: R )( implicit c: C, view: R => Ref[ C#V, T ]) : T = view( r ).access( c )
-      implicit def substitute[ C <: AnyCtx, T ]( r: AnyRf[ T ])( implicit c: C ) : Ref[ C#V, T ] = r.substitute( c )
+      implicit def access[ V <: Version, R, T ]( r: R )( implicit c: Ctx[ V ], view: R => Ref[ V, T ]) : T = view( r ).access( c )
+      implicit def substitute[ V <: Version, T ]( r: AnyRf[ T ])( implicit c: Ctx[ V ]) : Ref[ V, T ] = r.substitute( c )
    }
    trait Ref[ V1 <: Version, T ] {
       def access( implicit c: Ctx[ V1 ]) : T
-      def substitute[ C <: AnyCtx ]( implicit c: C ) : Ref[ C#V, T ]
+      def substitute[ V <: Version ]( implicit c: Ctx[ V ]) : Ref[ V, T ]
    }
 
    trait Factory {
-      def makeVar[ C <: AnyCtx, T ]( init: T )( implicit c: C ) : Ref[ C#V, T ]
+      def makeVar[ V <: Version, T ]( init: T )( implicit c: Ctx[ V ]) : Ref[ V, T ]
    }
 
-//   def shouldCompile1( r: AnyRf[ String ])( implicit c: AnyCtx ) : String = r
+   // def shouldCompile1[ V <: Version ]( r: AnyRf[ String ])( implicit c: Ctx[ V ]) : String = r
 
-   def shouldCompile2( r: AnyRf[ String ])( implicit c: AnyCtx ) : String = {
+   def shouldCompile2[ V <: Version ]( r: AnyRf[ String ])( implicit c: Ctx[ V ]) : String = {
       val r1 = Ref.substitute( r )
       r1.access( c )
    }
 
-//   def shouldFail( r: AnyRf[ String ])( implicit c: AnyCtx ) : String = r.access( c )
+   // def shouldFail[ V <: Version ]( r: AnyRf[ String ])( implicit c: Ctx[ V ]) : String = r.access( c )
 
 //   trait Var {
 //      def set[ C <: Ctx ]( r: Ref[ C#V, String ])( implicit c: C ) : Unit

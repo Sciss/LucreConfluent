19ef0cd43a7285d1ab4e49719bccdf249d196316
Sciss <contact@sciss.de>
2011-04-04 23:51:34 +0100

    ...

--- src/main/scala/de/sciss/confluent/test/ThreadedAccess.scala
+++ src/main/scala/de/sciss/confluent/test/ThreadedAccess.scala
@@ -2,79 +2,37 @@ package de.sciss.confluent.test
 
 // screw the type safety. doesn't seem to be possible with serialization etc. anyway
 object ThreadedAccess {
-   trait World[ Repr ]
-
-   trait Ctx {
-      type Next <: Ctx
+   trait System[ A <: Access[ _ ]] {
+      def in[ T ]( v: Version )( fun: A => T ) : T
    }
 
-//   trait State[ C1 ]
-   trait Ref[ C1 <: Ctx ] {
-      def set( link: Ref[ C1 ])( implicit c: C1 )
-      def get( implicit c: C1 ): (C1#Next, Ref[ C1#Next ])
-   }
-
-//   trait System[ W <: World[ W ]]
-   trait System {
-//      def makeRef[   W1 <: W ]( implicit w: W1 ) : Ref[ W1 ]
-//      def t[ W1 <: W, T ]( pred: W1 )( fun: W1#Next => T ) : T
-      def in( v: Version ) : Executer[ Access ]
-//      def inn[ V ]( v: V ) : Executer[ Access2[ V ]]
-   }
-
-   trait Executer[ W ] {
-      def apply[ T ]( fun: W => T ) : T
-   }
-
-//   object Access {
-//      implicit def self[ X <: Access2[ _ ]]( a: X ) = a
-//   }
-   trait Access {
-      def head : Kuku[ this.type ]
-      def head_=( r: Kuku[ this.type ] ) : Unit
-      def tail : Kuku[ this.type ]
-      def tail_=( r : Kuku[ this.type ] ) : Unit
+   trait Access[ Repr ] {
       def version : Version
-
-      def meld[ R[ _ ]]( v: Version )( fun: Access => RefX[ _, R ]) : R[ this.type ]
-   }
-
-//   def test[ C1 <: Ctx ]( r1: Ref[ C1 ])( implicit c1: C1 ) {
-//      val (c2, r2) = r1.get
-//      val (c3, r3) = r2.get
-//      r1.set( r3 )
-//   }
-
-   object Version {
-      def init: Version = error( "egal" )
+      def meld[ R[ _ ]]( v: Version )( fun: Repr => Ref[ _, R ]) : R[ this.type ]
    }
 
    trait Version
 
-   trait RefX[ A, Repr[ _ ]] {
+   trait Ref[ A, Repr[ _ ]] {
       def sub[ B ] : Repr[ B ]
    }
 
-   trait Kuku[ A ] extends RefX[ A, Kuku ]
+   trait MyRef[ A ] extends Ref[ A, MyRef ]
 
-//   trait Access extends World[ Access ] {
-//      def head : RefX
-//      def head_=( r: RefX ) : Unit
-//      def tail : RefX
-//      def tail_=( r : RefX ) : Unit
-//      def version : Version
-//   }
+   trait MyAccess extends Access[ MyAccess ] {
+      def head : MyRef[ this.type ]
+      def head_=( r: MyRef[ this.type ] ) : Unit
+      def tail : MyRef[ this.type ]
+      def tail_=( r : MyRef[ this.type ] ) : Unit
+   }
 
-   def gugu( sys: System, v1: Version ) {
-      val v0 = Version.init
+   def test( sys: System[ MyAccess ], v0: Version, v1: Version ) {
       val v2 = sys.in( v0 )( a => { a.tail = a.meld( v1 )( _.head ); a.version })
-//      val a0 = sys.inn[ v0.type ]( v0 ) { a => a }
-//      val a1 = sys.inn[ v1.type ]( v1 ) { a => a }
-      val a0 = sys.in( v0 ) { a => a }
-      val a1 = sys.in( v1 ) { implicit a =>
-         a.head = a.head // ( Access2.self[ a.type ]( a ))
-         a
+      val a3 = sys.in( v2 ) { a => a }
+      val (v4, a4) = sys.in( v1 ) { a =>
+         a.head = a.head
+         (a.version, a)
       }
-//      a1.head = a0.head
+      // a3.head = a4.head // forbidden
    }
 }

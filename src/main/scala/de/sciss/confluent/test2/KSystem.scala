92be30c0266c2f9cbbd81928937c4aae850362f6
Sciss <contact@sciss.de>
2011-04-07 01:08:46 +0100

    cannot uphold contravariance in access. una mierda.

--- src/main/scala/de/sciss/confluent/test2/KSystem.scala
+++ src/main/scala/de/sciss/confluent/test2/KSystem.scala
@@ -9,13 +9,13 @@ object Path {
 }
 trait Path
 
-trait Ref[ -A, Repr[ -_ ]] {
+trait Ref[ A, Repr[ _ ]] {
    def sub[ B ]( b: B ) : Repr[ B ]
 }
 
 trait Access[ Repr ] {
    def path : Path
-   def meld[ R[ -_ ]]( p: Path )( fun: Repr => Ref[ _, R ]) : R[ this.type ]
+   def meld[ R[ _ ]]( p: Path )( fun: Repr => Ref[ _, R ]) : R[ this.type ]
 }
 
 //trait AccessFactory[ A ] {
@@ -52,16 +52,21 @@ object Test {
    }
 
    object CLinkOption {
-      def empty[ A <: Access[ _ ], V ]( implicit a: A ) : CLinkOption[ A, V ] = error( "TODO" )
+      def empty[ A <: Access[ _ ], V ]( implicit a: A ) : CNoLink[ A, V ] = error( "TODO" )
+      def single[ A <: Access[ _ ], V ]( init: V )( implicit a: A ) : CLink[ A, V ] = error( "TODO" )
    }
-   trait Funk[ -A ]
-   sealed trait CLinkOption[ -A, V ] extends Ref[ A, ({type λ[-α] = CLinkOption[ α, V ]})#λ ] {
+   trait Funk[ A ]
+   sealed trait CLinkOption[ A, V ] extends Ref[ A, ({type λ[α] = CLinkOption[ α, V ]})#λ ] {
       def lift: Option[ CLink[ A, V ]]
    }
-   trait CLink[ -A, V ] extends CLinkOption[ A, V ] {
+   trait CLink[ A, V ] extends CLinkOption[ A, V ] {
+      def value: V
+      def value_=( v: V ) : Unit
+      def next: CLinkOption[ A, V ]
+      def next_=( l: CLinkOption[ A, V ]) : Unit
       def lift: Option[ CLink[ A, V ]] = Some( this )
    }
-   trait CNoLink[ -A, V ] extends CLinkOption[ A, V ] {
+   trait CNoLink[ A, V ] extends CLinkOption[ A, V ] {
       def lift: Option[ CLink[ A, V ]] = None
    }
 
@@ -71,10 +76,14 @@ object Test {
          val storeFactory  = new HashedStoreFactory[ Vertex ]
       }
 
-      val l1 = sys.in( Path.init ) { implicit a =>
-         a.head = CLinkOption.empty
-         a.head = CLinkOption.empty[ a.type, Int ]( a ) // works, too
-         a.head
+      val v1 = sys.in( Path.init ) { implicit a =>
+         val w0   = CLinkOption.single[ a.type, Int ]( 2 )( a )
+         val w1   = CLinkOption.single[ a.type, Int ]( 1 )( a )
+         w0.next  = w1
+         a.head   = w0 // CLinkOption.empty
+//         a.head   = CLinkOption.empty[ a.type, Int ]( a ) // works, too
+//         a.head
+         a.path
       }
 
       sys.in( Path.init ) { implicit a =>

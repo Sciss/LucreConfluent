730ba621fcd346149c76dda776835cc3dd9a4856
Sciss <contact@sciss.de>
2012-04-20 18:33:36 +0100

    ...

--- src/test/scala/de/sciss/confluent/EventMeld.scala
+++ src/test/scala/de/sciss/confluent/EventMeld.scala
@@ -1,16 +1,25 @@
 package de.sciss.confluent
 
-import de.sciss.lucre.{DataInput, DataOutput, event => evt}
-import de.sciss.lucre.stm.Serializer
 import collection.immutable.{IndexedSeq => IIdxSeq}
+import de.sciss.lucre.stm.{Cursor, Serializer}
+import de.sciss.lucre.{LucreSTM, DataInput, DataOutput, event => evt}
+import java.io.File
+import de.sciss.lucre.stm.impl.BerkeleyDB
 
-object EventMeld {
+object EventMeld extends App {
+   LucreSTM.showEventLog   = true
+   val dir                 = File.createTempFile( "database", "db" )
+   dir.delete()
+   val store               = BerkeleyDB.factory( dir )
+   implicit val s          = Confluent( store )
+   val p = new EventMeld[ Confluent ]
+   p.run()
 }
 class EventMeld[ S <: KSys[ S ]] {
    implicit def seqSer[ A ]( implicit peer: Serializer[ A ]) : Serializer[ IIdxSeq[ A ]] = Serializer.indexedSeq[ A ]
 
    object Group extends evt.Decl[ S, Group ] {
-      val serializer : evt.Reader[ S, Group ] = Ser
+      implicit val serializer : evt.NodeSerializer[ S, Group ] = Ser
 
       declare[ Update ]( _.collectionChanged )
 
@@ -20,34 +29,36 @@ class EventMeld[ S <: KSys[ S ]] {
 
       def empty( implicit tx: S#Tx ) : Group = new Group {
          protected val targets = evt.Targets[ S ]
-         protected val children  = tx.newVar[ IIdxSeq[ Child ]]( targets.id, IIdxSeq.empty )
+         protected val childrenVar  = tx.newVar[ IIdxSeq[ Child ]]( targets.id, IIdxSeq.empty )
       }
 
-      private object Ser extends evt.Reader[ S, Group ] {
+      private object Ser extends evt.NodeSerializer[ S, Group ] {
          def read( in: DataInput, access: S#Acc, _targets: evt.Targets[ S ])( implicit tx: S#Tx ) : Group = new Group {
             protected val targets   = _targets
-            protected val children  = tx.readVar[ IIdxSeq[ Child ]]( id, in )
+            protected val childrenVar  = tx.readVar[ IIdxSeq[ Child ]]( id, in )
          }
       }
    }
    trait Group extends evt.Compound[ S, Group, Group.type ] {
-      protected def children: S#Var[ IIdxSeq[ Child ]]
-      final protected def decl = Group
+      protected def childrenVar: S#Var[ IIdxSeq[ Child ]]
+      protected def decl = Group
 
-      final lazy val collectionChanged : evt.Trigger[ S, Group.Update, Group ] = event[ Group.Update ]
+      lazy val collectionChanged : evt.Trigger[ S, Group.Update, Group ] = event[ Group.Update ]
 
       def add( c: Child* )( implicit tx: S#Tx ) {
          val seq = c.toIndexedSeq
-         children.transform( _ ++ seq )
+         childrenVar.transform( _ ++ seq )
          collectionChanged( Group.Added( this, seq ))
       }
 
-      final protected def disposeData()( implicit tx: S#Tx ) {
-         children.dispose()
+      def elements( implicit tx: S#Tx ) : IIdxSeq[ Child ] = childrenVar.get
+
+      protected def disposeData()( implicit tx: S#Tx ) {
+         childrenVar.dispose()
       }
 
-      final protected def writeData( out: DataOutput ) {
-         children.write( out )
+      protected def writeData( out: DataOutput ) {
+         childrenVar.write( out )
       }
    }
 
@@ -80,5 +91,32 @@ class EventMeld[ S <: KSys[ S ]] {
    }
    trait Child {
       def name: String
+      override def toString = "Child(" + name + ")"
+   }
+
+   def run()( implicit system: S, cursor: Cursor[ S ]) {
+      val groupAcc = system.root( Group.empty( _ ))
+      cursor.step { implicit tx =>
+         groupAcc.get.collectionChanged.reactTx { implicit tx =>
+            (e: Group.Update) => println( "____OBSERVE____ " + e )
+         }
+      }
+
+      def group( implicit tx: S#Tx ) = groupAcc.get
+
+      val v0 = cursor.step { implicit tx =>
+         group.add( Child( "A" ))
+         tx.inputAccess
+      }
+
+      val v1 = cursor.step( _.inputAccess )
+
+      cursor.step { implicit tx =>
+         group.add( groupAcc.meld( v1 ).elements.head )
+      }
+
+      println( "Children = " + cursor.step { implicit tx =>
+         group.elements
+      })
    }
 }

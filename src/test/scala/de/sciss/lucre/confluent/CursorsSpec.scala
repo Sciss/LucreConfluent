7797f41bd604fff8e3c02186eea9a5a009b6d4a5
Sciss <contact@sciss.de>
2012-10-21 18:23:06 +0200

    ok... let's forget about durable for the moment, it should be fine to have the cursors in confluent var for the piece.

--- src/test/scala/de/sciss/lucre/confluent/CursorsSpec.scala
+++ src/test/scala/de/sciss/lucre/confluent/CursorsSpec.scala
@@ -18,9 +18,9 @@ class CursorsSpec extends fixture.FlatSpec with ShouldMatchers {
 //   confluent.showLog = true
 
    object Entity {
-      implicit object CursorSer extends stm.Serializer[ D#Tx, D#Acc, Cursor[ S ]] {
-         def write( c: Cursor[ CursorsSpec.this.type#S ], out: DataOutput ) { c.write( out )}
-         def read( in: DataInput, access: D#Acc)( implicit tx: D#Tx ) : Cursor[ S ] = {
+      implicit object CursorSer extends stm.Serializer[ S#Tx, S#Acc, Cursor[ S ]] {
+         def write( c: Cursor[ S ], out: DataOutput ) { c.write( out )}
+         def read( in: DataInput, access: S#Acc)( implicit tx: S#Tx ) : Cursor[ S ] = {
             tx.readCursor( in, access )
          }
       }
@@ -28,9 +28,10 @@ class CursorsSpec extends fixture.FlatSpec with ShouldMatchers {
       implicit object Ser extends MutableSerializer[ S, Entity ] {
          protected def readData( in: DataInput, id: S#ID )( implicit tx: S#Tx ) = {
             val field      = tx.readIntVar( id, in )
-            val dtx: D#Tx  = tx.durable
-            val did        = dtx.readID( in, () )
-            val cursors    = dtx.readVar[ IIdxSeq[ Cursor[ S ]]]( did, in )
+//            val dtx: D#Tx  = tx.durable
+//            val did        = dtx.readID( in, () )
+//            val cursors    = dtx.readVar[ IIdxSeq[ Cursor[ S ]]]( did, in )
+            val cursors    = tx.readVar[ IIdxSeq[ Cursor[ S ]]]( id, in )
             new Entity( id, field, cursors )
          }
       }
@@ -38,23 +39,24 @@ class CursorsSpec extends fixture.FlatSpec with ShouldMatchers {
       def apply( init: Int )( implicit tx: S#Tx ) : Entity = {
          val id         = tx.newID()
          val field      = tx.newIntVar( id, init )
-         val dtx: D#Tx  = tx.durable
-         val did        = dtx.newID()
+//         val dtx: D#Tx  = tx.durable
+//         val did        = dtx.newID()
          val initCsr    = IIdxSeq( tx.newCursor( tx.inputAccess ), tx.newCursor( tx.inputAccess ) )
-         val cursors    = dtx.newVar[ IIdxSeq[ Cursor[ S ]]]( did, initCsr )
+//         val cursors    = dtx.newVar[ IIdxSeq[ Cursor[ S ]]]( did, initCsr )
+         val cursors    = tx.newVar[ IIdxSeq[ Cursor[ S ]]]( id, initCsr )
          new Entity( id, field, cursors )
       }
    }
-   class Entity( val id: S#ID, val field: S#Var[ Int ], cursorsVar: D#Var[ IIdxSeq[ Cursor[ S ]]])
+   class Entity( val id: S#ID, val field: S#Var[ Int ], cursorsVar: S#Var[ IIdxSeq[ Cursor[ S ]]])
    extends stm.Mutable.Impl[ S ] {
       protected def disposeData()( implicit tx: S#Tx ) {
-         implicit val dtx: D#Tx  = tx.durable
+//         implicit val dtx: D#Tx  = tx.durable
          field.dispose()
          cursorsVar.dispose()
       }
 
       def cursors( implicit tx: S#Tx ) : IIdxSeq[ Cursor[ S ]] = {
-         implicit val dtx: D#Tx  = tx.durable
+//         implicit val dtx: D#Tx  = tx.durable
          cursorsVar.get
       }
 

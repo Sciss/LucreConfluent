03530e803d188142cc03a65db5d2e58021a354d3
Sciss <contact@sciss.de>
2012-10-28 23:22:21 +0100

    make sure event data store is clear when opening

--- src/main/scala/de/sciss/lucre/confluent/reactive/impl/ConfluentReactiveImpl.scala
+++ src/main/scala/de/sciss/lucre/confluent/reactive/impl/ConfluentReactiveImpl.scala
@@ -284,7 +284,7 @@ object ConfluentReactiveImpl {
       def durableTx(  tx: S#Tx ) = tx.durable
       def inMemoryTx( tx: S#Tx ) = tx.inMemory
 
-      private val eventStore  = storeFactory.open( "event" )
+      private val eventStore  = storeFactory.open( "event", overwrite = true )
       private val eventVarMap = DurablePersistentMap.newConfluentIntMap[ S ]( eventStore, this, isOblivious = true )
       val eventCache : CacheMap.Durable[ S, Int, DurablePersistentMap[ S, Int ]] = DurableCacheMapImpl.newIntCache( eventVarMap )
 
